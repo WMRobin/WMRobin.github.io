46bef73e9c26380ec3b777174974e6a5dad95b8f
WMRobin <RobinsonWM17@gmail.com>
2025-03-20 09:40:45 -0400

    clean up code

--- public-goods/serial-dictator-program/serial-dictator-program.jl
+++ public-goods/serial-dictator-program/serial-dictator-program.jl
@@ -1,21 +1,24 @@
 using CSV
-# using DataFrames
+using DataFrames
 import Combinatorics
 import Random
 
 # Set the seed for reproducibility
-Random.seed!(42)
+Random.seed!(42);
 
 # set your directories
-indir = "./"
-outdir = "./"
+indir = "./";
+outdir = "./";
+
+# name of csv file
+filename = "example-preferences.csv";
 
 # ===================================== #
 # =============== SETUP =============== #
 # ===================================== #
 
 # read data
-prefs = CSV.read(indir * "example-preferences.csv", DataFrames.DataFrame);
+prefs = CSV.read(indir * filename, DataFrame);
 
 # identify individuals
 individuals = collect(names(prefs));
@@ -88,7 +91,7 @@ max_payoff = length(objects) + 1;
 
 # allocate the goods to the individuals in all possible orders
 allocations = Dict();
-for order in collect(permutations(1:length(individuals)))
+for order in collect(Combinatorics.permutations(1:length(individuals)))
     println("order: $order")
     allocations[order] = serial_dictator(preferences, objects, order, surplus_people)
     println("")
@@ -107,15 +110,23 @@ end
 
 # identify the orders that give maximum payoff
 max_orders = orders[findall(x -> payoffs[x] .== maximum(values(payoffs)), orders)];
+# if more than one, return all? default is to use lottery to choose one
+return_all = false;
 if length(max_orders) == 1
     # if there is a unique optimal order, choose it
     optimal_order = max_orders[1]
     optimal_allocation = allocations[optimal_order]
-elseif length(max_orders) > 1
+elseif length(max_orders) > 1 && return_all
+    # if there are multiple optimal orders, return all
+    optimal_allocation = Dict()
+    for order in max_orders
+        optimal_allocation[order] = allocations[order]
+    end
+elseif length(max_orders) > 1 && !return_all
     # if there are multiple optimal orders, choose one at random
     optimal_order = max_orders[rand(1:length(max_orders))]
     optimal_allocation = allocations[optimal_order]
 end
 
 # save the optimal allocation to a CSV file
-CSV.write(outdir * "serial-dictator-allocation.csv", DataFrames.DataFrame(optimal_allocation));
+CSV.write(outdir * "serial-dictator-allocation.csv", DataFrame(optimal_allocation));
